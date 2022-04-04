6042fd4032a3cc2b706877275d471dcbbf047ac6
PG-DESD <tarun374374@gmail.com>
2022-04-04 11:04:43 +0530

    Update main.c

--- ARM/Lab/Demo_InlineAssembly_006/Src/main.c
+++ ARM/Lab/Demo_InlineAssembly_006/Src/main.c
@@ -17,15 +17,21 @@ int main(void)
 	__asm volatile("ADD R0,R0,R1");
 	__asm volatile("STR R0,[R2]");
 #endif
-
+	/*Example1: Writing C Var to Assembly*/
 	/* store 'val' in to R0*/
 	int val=50;
 	__asm volatile("MOV R0,%0": :"r"(val));
+	//__asm volatile("Code template: o/p var: i/p var")
+	//__asm volatile("ADD %0,%1,%2":"=r"(res):"r"(var1),"r"(var2));
+	//-----------------------------------%0--------%1---------%2----
 
-	/*read CONTROL register value in to control_reg vriable */
+	/*Example2: Reading Assembly to C Var*/
+	/*read CONTROL register value in to control_reg variable */
 	int control_reg;
 	__asm volatile("MRS %0,CONTROL": "=r"(control_reg) );
+	//----------------------------------------%0---------
 
+	/*Example3: Using C Pointers and Assembly*/
 	//Operands are referenced by a percent sign followed by a single digit,
 	//where %0 refers to the first %1 to the second operand and so forth.
 
@@ -35,6 +41,10 @@ int main(void)
 	p2 = (int*)0x20000008;
 
 	__asm volatile("LDR %0,[%1]": "=r"(p1): "r"(p2));
+
+	/*Practice*/
+	int rel=0,var1=10,var2=20;
+	__asm volatile("ADD %0,%1,%2":"=r"(rel):"r"(var1),"r"(var2));
 
 
 	for(;;);
